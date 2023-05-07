63fba0141d8f74f9c669b423b684b760df5fd7b9
Molnár Dávid <d.molnarka@hotmail.com>
2023-05-07 14:52:15 +0200

    Add operate function
    This function decides which arithmetic function to call based on the
    operator

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let operand1, operator, operand2;
+
 function add(x, y) {
     return x + y;
 }
@@ -14,4 +16,26 @@ function divide(x, y) {
     return x / y;
 }
 
+function operate(operand1, operator, operand2) {
+    switch (operator) {
+        case "+":
+            return add(+operand1, +operand2);
+            break;
+        case "-":
+            return subtract(+operand1, +operand2);
+            break;
+        case "*":
+            return multiply(+operand1, +operand2);
+            break;
+        case "/":
+            return divide(+operand1, +operand2);
+            break;
+        default:
+            return;
+    }
+}
 
+operand1 = prompt("Enter operand1:");
+operator = prompt("Enter operator:");
+operand2 = prompt("Enter operand2:");
+console.log(operate(operand1, operator, operand2));
